27768b410fe510c33cd80b3823f7fc99940cd8f2
Zailant <alvarojose1595@gmail.com>
2022-08-18 20:51:29 -0500

    v2-Agregamos Productos

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author ALVARO MONROY
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Probando Repositorio");
+        System.out.println("Probando Repositorio Segunda Version");
+        
     }
     
 }
